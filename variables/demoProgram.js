14738c12653964c3fde8ddd5c55e198d29d1444e
ajaicrocier <ajaicrocier@gmail.com>
2021-09-09 14:19:18 +0530

    1 - variables demo programs

---
+++ variables/demoProgram.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 89.5;
+var firstName = "Ajai";
+var lastName = "Crocier";
+console.log(found);
+console.log(grade);
+console.log("hi!" + " " + firstName + lastName);
+//using template string
+console.log("Hi " + firstName + " " + lastName);
---
+++ variables/demoProgram.ts
@@ -0,0 +1,12 @@
+let found:boolean = true;
+let grade : number = 89.5;
+let firstName : string = "Ajai";
+let lastName : string = "Crocier";
+
+console.log(found);
+console.log(grade);
+console.log("hi!"+" " + firstName + lastName);
+
+//using template string
+
+console.log(`Hi ${firstName} ${lastName}`);
